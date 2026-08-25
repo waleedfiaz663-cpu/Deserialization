e2300df30a57de5cbd8963a8f322863d5b5d3292
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-25 19:29:09 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[]args){
+        
+        try{
+            
+            Student S = new Student("Waleed",23);
+            
+            Student S1 =new Student("Ahsan",22);
+            
+            FileInputStream fis = new FileInputStream("C:/Users/Administrator/OneDrive/Desktop/Student.Ser");
+            
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            
+            Student s = (Student) ois.readObject();
+            
+            Student s1 = (Student) ois.readObject();
+            
+            ois.close();
+                    
+        }
+    
+        
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    
+}
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    
+    String name;
+    int age;
+    
+public Student(String name,int age){
+    
+    this.name=name;
+    this.age=age;
+}    
+
+public String toString(){
+    return "Name : " +name+ "\n age :" +age;
+}
+    
+}
