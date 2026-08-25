f9d4c427e0db0076a06cd5aace6cd0dfe5ccf149
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-25 19:29:41 +0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,10 +9,13 @@ public Student(String name,int age){
     
     this.name=name;
     this.age=age;
+    
 }    
 
 public String toString(){
+    
     return "Name : " +name+ "\n age :" +age;
+    
 }
     
 }
